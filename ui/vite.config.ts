7f25180c421879a2394f19907dfb82bc5b946b1c
Shane O'Donnell <shane.odonnell@steeltoad.io>
2022-07-05 14:43:36 -0400

    revert vite changes

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,25 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import externalGlobals from "rollup-plugin-external-globals";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    externalGlobals({
-      react: "React"
-    })],
+  plugins: [react()],
   envDir: '../environments',
   envPrefix: 'REA_PLAYSPACE_',
   server: {
       port: 8080
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        'react'
-      ],
-    }
   }
 })
